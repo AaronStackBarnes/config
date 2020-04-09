cf541d0836aefb1c2fea4d6826ea9bb99d74aba5
Aaron Stack <aaron.stack@area23hc.com>
2020-04-09 17:11:52 -0400

    update

--- save.sh
+++ save.sh
@@ -1,7 +1,9 @@
+echo "$SHELL"
+
 git add -A
 git commit -am "update"
 git push 
 
 cp ~/websites/config/zshrc ~/.zshrc 
 cp ~/websites/config/vimrc ~/.vimrc
-echo "vimrc and .zshrc not auto loading"
+echo "vimrc"
